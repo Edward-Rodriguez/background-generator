c275b19cd43982c477aa1e051bc5f16db02cbfed
Edward Roddriguez <edwardR36@hotmail.com>
2019-12-30 17:34:53 -0500

    add degree picker to change orientation of gradient add degree dropdown make default gradient same as inputs on loadpage

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+var css = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.getElementById('gradient');
+var degree = document.querySelector('select[name=degrees]');
+var defaultColor1 = '#0000ff';
+var defaultColor2 = '#ff0000';
+var defaultDegree = '90';
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+console.log(degree);
 
-	css.textContent = body.style.background + ";";
+function setGradientColors(firstColor, secondColor, deg) {
+  body.style.background =
+    'linear-gradient(' + deg + 'deg,' + firstColor + ', ' + secondColor + ')';
+  css.textContent = body.style.background + ';';
 }
 
-color1.addEventListener("input", setGradient);
+function setGradient() {
+  var deg = degree.options[degree.selectedIndex].value;
+  setGradientColors(color1.value, color2.value, deg);
+}
 
-color2.addEventListener("input", setGradient);
+function startup() {
+  setGradientColors(defaultColor1, defaultColor2, defaultDegree);
+  color1.value = defaultColor1;
+  color2.value = defaultColor2;
+  color1.addEventListener('input', setGradient);
+  color2.addEventListener('input', setGradient);
+  degree.addEventListener('change', setGradient);
+}
+
+startup();
