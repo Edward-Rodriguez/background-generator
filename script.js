bbc59b6b90244c6d3355b88ce1b28545ce1c531f
Edward Rodriguez <edwardR36@hotmail.com>
2020-02-04 16:10:12 -0500

    install lodash and browserify

--- script.js
+++ script.js
@@ -9,6 +9,12 @@
  * @since 1/4/20
  * */
 
+var _ = require('lodash');
+console.log(_);
+
+var array = [1, 2, 3, 4];
+console.log('answer:', _.without(array, 3));
+
 var css = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
